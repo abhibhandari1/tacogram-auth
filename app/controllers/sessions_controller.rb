cfeb13d5fa8b91f90ed52acad15ca910cb3549ec
abhibhandari1 <abhishek.bhandari@kellogg.northwestern.edu>
2022-05-30 16:39:07 +0000

    -

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,27 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
+    @user = User.find_by({"email" => params["email"] })
+    if @user
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+            session["user_id"] = @user["id"]
+            flash["notice"] = "Welcome"
+            redirect_to "/posts"
+        else
+            flash["notice"] = "Password Incorrect"
+            redirect_to "/sessions/new"
+        end
+    else
+    flash["notice"] = "User does not exist"
+    redirect_to "/sessions/new"
+    end
+    
   end
+  
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye"
+    redirect_to "/sessions/new"
+end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,15 @@ class UsersController < ApplicationController
     @user["first_name"] = params["user"]["first_name"]
     @user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
-    @user.save
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
+        
+    if User.exists?("email" => @user["email"])
+      flash["notice"] = "User ID already exists"
+    else
+      flash["notice"] = "Welcome to Tacogram"
+      @user.save      
+    end
+    
     redirect_to "/posts"
   end
 end
