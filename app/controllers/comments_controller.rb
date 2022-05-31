2ed18b1faa94e5271b37d6b2c2a533e29519930e
abhibhandari1 <abhishek.bhandari@kellogg.northwestern.edu>
2022-05-31 00:03:39 +0000

    -

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,17 @@
 class CommentsController < ApplicationController
 
+    def new
+        @comment = Comment.new
+        @comment["post_id"] = params["post_id"]
+    end
+
     def create
         @comment = Comment.new
-        @comment["contact_id"] = params["comment"]["contact_id"]
+        @comment["post_id"] = params["comment"]["post_id"]
         @comment["comment"] = params["comment"]["comment"]
         @comment["user_id"] = @current_user["id"]
         @comment.save
-        redirect_to "/posts/#{@current_user["user_id"]}"
+        redirect_to "/posts"
     end
     
 
